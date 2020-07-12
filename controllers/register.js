6b9ba5f40d313d55b1d6e595dd5794b5753effa6
code.developer13 <dev13@codecraft.com>
2020-07-12 16:43:10 -0400

    -Primer commit -Login y regsiter terminados (esperando po cambios en BD) -se Añadieron los futuros archivos de los crud para doorway

---
+++ controllers/register.js
@@ -0,0 +1,34 @@
+const handleRegister = (db, bcrypt)=> (req,res) =>{
+    const { email, password, name} = req.body;
+
+    if(!email || !password || !name){
+        return res.status(400).json('Formulario Llenado Incorrectamente')
+    }
+
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password,salt);
+    console.log(hash.length)
+
+    db.transaction(trx => {
+        trx.insert({
+            password:hash,
+            username: name,
+            NombreCompleto: email
+        })
+        .into('users')
+        .then(id=>{
+            return trx('users')
+            .where({id:id[0]})
+            .then(user => {
+                res.json(user[0].id)
+            })
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
+    })
+    .catch(err => res.status(400).json('No se ha podido registrar'))
+}
+
+module.exports ={
+    handleRegister: handleRegister
+}
