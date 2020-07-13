662d18947f3a49319cf442067bcd22f8a9a7a3e8
code.developer13 <dev13@codecraft.com>
2020-07-12 20:21:12 -0400

    -se termino crud de clientes y producto -template de controlador añadidos a factura serial y reporte

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
+//Lib imports
 const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require("bcrypt-nodejs")
 const cors = require('cors');
 const knex = require('knex');
 
+//user manage
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
+
+//client manage
+const client = require('./controllers/cliente');
+
+//producto manage
+const producto = require('./controllers/producto');
 
 const db = knex({
   client: 'mysql',
@@ -36,11 +44,30 @@ app.post('/signin', signin.handleSignin(db,bcrypt));
 
 app.post('/register', register.handleRegister(db, bcrypt));
 
-// app.get('/profile/:id', profile.handleProfileGet(db));
+//cliente
 
-// app.put('/image', image.handleImage(db));
+app.get('/client-list', client.handleList(db));
 
-// app.post('/imageurl', image.handleApiCall);
+app.get('/client-get/:id', client.handleGet(db));
+
+app.post('/client-create', client.handleCreate(db));
+
+app.post('/client-update', client.handleUpdate(db));
+
+app.post('/client-remove', client.handleRemove(db));
+
+//producto
+
+app.get('/producto-list', producto.handleList(db));
+
+app.get('/producto-get/:id', producto.handleGet(db));
+
+app.post('/producto-create', producto.handleCreate(db));
+
+app.post('/producto-update', producto.handleUpdate(db));
+
+app.post('/producto-remove', producto.handleRemove(db));
+
 
 
 app.listen( 3000, () => console.log('app is runnning on port '+ 3000));
